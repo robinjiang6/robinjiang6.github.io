f865f45634f295a2ab26d7afef46afc6878e83a3
Robin Jiang <robinjiang6@gmail.com>
2022-06-11 19:29:47 -0700

    change message/testing workflow

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.DDDDDDDD
+          <h1> BRUH BRUH POGGERS CHAMPION I THINK POGGERS</h1>
         </p>
         <a
           className="App-link"
