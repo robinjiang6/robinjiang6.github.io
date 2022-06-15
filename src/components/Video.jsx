67ad408404ff6086d3916b30a5aaa234c8f25f9c
Robin Jiang <robinjiang6@gmail.com>
2022-06-15 12:26:14 -0700

    start work on homepage

---
+++ src/components/Video.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Video.css'
+
+function Video() {
+  return (
+    <div className='video-container'>
+        <img src="/videos/ponyo.gif" alt='ponyo gif' id="home-screen"></img>
+        <h1>hi there</h1>
+    </div>
+  )
+}
+
+export default Video
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import '../../App.css';
+import Video from '../Video'
+import React from 'react'
+
+function Home() {
+    return(
+        <div>
+            <Video />
+        </div>
+    ); 
+}
+
+export default Home;
