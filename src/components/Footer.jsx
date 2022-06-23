2c8083acae906a38eafda1ed333e662deac6964c
Robin Jiang <robinjiang6@gmail.com>
2022-06-23 12:55:43 -0700

    changed text in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
           <div className="links">
             <a href="https://github.com/robinjiang6" target="_blank" rel="noreferrer" className="github-container">
               <img src="/images/github-64.png" alt="Github Logo-" id="github-img"></img>
-              <text className="github-text">Check Out my Github</text>
+              <text className="github-text">My Github</text>
             </a>
             <a href="https://www.linkedin.com/in/robin-jiang-805249202/" target="_blank" rel="noreferrer" className="linked-in-container">
               <img src="/images/linked-in-64.png" alt="Linked In Logo-" id="linked-in-img"></img>
