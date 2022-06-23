0b057d9c269bf322f7a81e98674cfca762122554
Robin Jiang <robinjiang6@gmail.com>
2022-06-23 14:02:37 -0700

    started about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import '../../App.css';
+import './About.css';
 function About() {
   return (
-    <div>About</div>
+    <div className='about'>
+      <div className='about-container'>
+        <img src='/images/me-pic.png' alt="me graduating-" className="about-picture"></img>
+        <div className="about-text">Hey there! I’m Robin, the owner of this (hopefully cool) website. I recently graduated from Troy High School, and will be studying Computer Engineering next year at UCI. In my free time I like coding, 3D printing, and of course gaming (im immortal peak 16 RR, going to be hardstuck ascendant in Valorant). I’ve also started working out and cooking steaks &#128523;. Hopefully you'll find this website interesting!
+        </div>
+      </div>
+    </div>
   )
 }
 
