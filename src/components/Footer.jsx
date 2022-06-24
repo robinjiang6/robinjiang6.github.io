382f34c17cdc2e1d4b4724d22a391c98e5702adf
Robin Jiang <robinjiang6@gmail.com>
2022-06-24 12:52:56 -0700

    start work on three js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,11 @@ function Footer() {
           <div className="links">
             <a href="https://github.com/robinjiang6" target="_blank" rel="noreferrer" className="github-container">
               <img src="/images/github-64.png" alt="Github Logo-" id="github-img"></img>
-              <text className="github-text">My Github</text>
+              <p className="github-text">My Github</p>
             </a>
             <a href="https://www.linkedin.com/in/robin-jiang-805249202/" target="_blank" rel="noreferrer" className="linked-in-container">
               <img src="/images/linked-in-64.png" alt="Linked In Logo-" id="linked-in-img"></img>
-              <text className="linkedin-text">My LinkedIn</text>
+              <p className="linkedin-text">My LinkedIn</p>
             </a>
           </div>
         </div>
