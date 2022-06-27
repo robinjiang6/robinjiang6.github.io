02befba34b40a1eeb253faa32a7dace5a6f461bc
Robin Jiang <robinjiang6@gmail.com>
2022-06-27 14:16:45 -0700

    swapped to home video, compressed about image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,10 +5,12 @@ function Home() {
     return(
         <div className='home'>
             <div className='home-container'>
-                <div className='mesh-container'>
+                <p className='title'>Welcome to my Website</p>
+                <div className='video-container'>
+                    <video src='/videos/portal.mp4' autoPlay loop muted className='video'/>
                 </div>
+                <p className='caption'>A looped video of one of my 3D designs</p>
             </div>
-            hello, the home page is under construction
         </div>
     ); 
 }
