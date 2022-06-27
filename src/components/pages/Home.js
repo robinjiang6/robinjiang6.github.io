a37f90edcbeadc75b8c91478f14cca7619a6e94f
Robin Jiang <robinjiang6@gmail.com>
2022-06-27 14:45:25 -0700

    work on home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,9 +6,7 @@ function Home() {
         <div className='home'>
             <div className='home-container'>
                 <p className='title'>Welcome to my Website</p>
-                <div className='video-container'>
-                    <video src='/videos/portal.mp4' autoPlay loop muted className='video'/>
-                </div>
+                <video src='/videos/portal-min.mp4' autoPlay loop muted className='video'/>
                 <p className='caption'>A looped video of one of my 3D designs</p>
             </div>
         </div>
