ebafc9bb1507d08400951a8200ef2a030cb587f0
Robin Jiang <robinjiang6@gmail.com>
2022-06-27 14:54:58 -0700

    added alt

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,7 @@ function Home() {
             <div className='home-container'>
                 <p className='title'>Welcome to my Website</p>
                 <video src='/videos/portal-min.mp4' autoPlay loop muted className='video'/>
-                <img src='/images/portal.png' className='image-portal'></img>
+                <img src='/images/portal.png' className='image-portal' alt='one of my 3D designs'></img>
                 <p className='caption'>A looped video of one of my 3D designs</p>
                 <p className='caption2'>A picture of one of my 3D designs</p>
             </div>
