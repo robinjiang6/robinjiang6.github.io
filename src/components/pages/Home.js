31137ee73396d6bd8242050ceb958ca4ae335b20
Robin Jiang <robinjiang6@gmail.com>
2022-11-24 20:36:14 -0800

    started working on three.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,11 @@
 import '../../App.css';
 import './Home.css';
-import React, {useEffect, useState} from 'react';
+//import React, {useEffect, useState} from 'react';
+import React from 'react';
+import ThreeD from '../ThreeD';
 
 function Home() {
+  /*
     const [mobile, setMobile] = useState(false);
     const [rotate, setRotate] = useState(false);
     
@@ -28,15 +31,18 @@ function Home() {
 
       window.addEventListener('resize', showVideo);
       window.addEventListener('resize', rotateImg);
-
+*/
     return(
         <div className='home'>
             <div className='home-container'>
                 <p className='title'>Welcome to my Website</p>
-                <video src={mobile ? 'data:,':'/videos/portal-min.mp4'} autoPlay loop muted className='video' alt='was a video'/>
-                <img src={rotate ? '/images/portal-rotated.png':'/images/portal.png'} className='image-portal' alt='one of my 3D designs'></img>
+                <ThreeD />
+                {/*
                 <p className='caption'>A looped video of one of my 3D designs</p>
                 <p className='caption2'>A picture of one of my 3D designs</p>
+                <video src={mobile ? 'data:,':'/videos/portal-min.mp4'} autoPlay loop muted className='video' alt='was a video'/>
+                <img src={rotate ? '/images/portal-rotated.png':'/images/portal.png'} className='image-portal' alt='one of my 3D designs'></img>
+                */}
             </div>
         </div>
     ); 
