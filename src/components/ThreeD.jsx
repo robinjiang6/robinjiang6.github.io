17440f5e0c8685cef37600add4c7b61b8203733a
Robin Jiang <robinjiang6@gmail.com>
2022-11-24 21:38:42 -0800

    got model to work, :D

--- src/components/ThreeD.jsx
+++ src/components/ThreeD.jsx
@@ -1,31 +1,30 @@
 import "./ThreeD.css";
 import { Canvas } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
-import { Environment, OrbitControls } from "@react-three/drei";
+import {OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Suspense } from "react";
 
 const Model = () => {
-  const gltf = useLoader(GLTFLoader, "./models/rmpg.gltf");
+  const gltf = useLoader(GLTFLoader, "./models/rmpg_centered.gltf");
   return (
     <>
-      <primitive object={gltf.scene} scale={0.4} />
+      <primitive object={gltf.scene} scale={30} />
     </>
   );
 };
 
 export default function ThreeD() {
-  return (
+  return(
     <div className="canvas-container">
-    <>
       <Canvas className='canvas'>
         <Suspense fallback={null}>
+          <ambientLight intensity={0.2} />
+          <directionalLight color="white" position={[0, 0, 5]} />
           <Model />
-          <OrbitControls />
-          <Environment preset="sunset" background />
+          <OrbitControls autoRotate autoRotateSpeed={0} enableZoom={false} enablePan={false} minPolarAngle={1} maxPolarAngle={2}/>
         </Suspense>
       </Canvas>
-    </>
     </div>
   );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,48 +1,15 @@
 import '../../App.css';
 import './Home.css';
-//import React, {useEffect, useState} from 'react';
 import React from 'react';
 import ThreeD from '../ThreeD';
 
 function Home() {
-  /*
-    const [mobile, setMobile] = useState(false);
-    const [rotate, setRotate] = useState(false);
-    
-    const showVideo = () => {
-        if(window.innerWidth <= 960){
-          setMobile(true);
-        }else{
-          setMobile(false);
-        }
-      };
-
-    const rotateImg = () => {
-        if(window.innerWidth <= 700){
-          setRotate(true);
-        }else{
-          setRotate(false);
-        }
-      };
-      useEffect(() =>{
-        showVideo();
-        rotateImg();
-      }, []);
-
-      window.addEventListener('resize', showVideo);
-      window.addEventListener('resize', rotateImg);
-*/
     return(
         <div className='home'>
             <div className='home-container'>
-                <p className='title'>Welcome to my Website</p>
+                <p className='home-title'>Welcome to my Website</p>
                 <ThreeD />
-                {/*
-                <p className='caption'>A looped video of one of my 3D designs</p>
-                <p className='caption2'>A picture of one of my 3D designs</p>
-                <video src={mobile ? 'data:,':'/videos/portal-min.mp4'} autoPlay loop muted className='video' alt='was a video'/>
-                <img src={rotate ? '/images/portal-rotated.png':'/images/portal.png'} className='image-portal' alt='one of my 3D designs'></img>
-                */}
+                <p className='home-caption'>A model of one of my 3D designs</p>
             </div>
         </div>
     ); 
