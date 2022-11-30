798ad38f06c2bbc7a1ffef351ab8c056011d7dab
Robin Jiang <robinjiang6@gmail.com>
2022-11-29 22:13:50 -0800

    started working on model

--- src/components/ThreeD.jsx
+++ src/components/ThreeD.jsx
@@ -1,27 +1,31 @@
 import "./ThreeD.css";
 import { Canvas } from "@react-three/fiber";
-import { useLoader } from "@react-three/fiber";
+import { useLoader} from "@react-three/fiber"; //, useThree } from "@react-three/fiber";
 import {OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Suspense } from "react";
 
 
 const Model = () => {
-  const windowWidth = Math.min(window.innerWidth, 900);
   const gltf = useLoader(GLTFLoader, "./models/rmpg_centered.gltf");
   return (
     <>
-      <primitive object={gltf.scene} scale={30 * windowWidth/900} />
+      <primitive object={gltf.scene} scale={30} />
     </>
   );
 };
+/*
+const UpdateState = () => {
+  const state = useThree()
+  state.camera.maxPolarAngle:3;
+}*/
 
 export default function ThreeD() {
-  window.addEventListener('resize', Model)
   return(
     <div className="canvas-container">
       <Canvas className='canvas'>
         <Suspense fallback={null}>
+          {/*<UpdateState />*/}
           <ambientLight intensity={0.2} />
           <directionalLight color="white" position={[0, 0, 5]} />
           <Model />
