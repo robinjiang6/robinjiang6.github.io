97f71a0b7efd2fcc9c1b803f494ab66621bbc4b8
Robin Jiang <robinjiang6@gmail.com>
2022-12-02 20:41:19 -0800

    changed orbitcontrols to presentation controls. Still need to limit zooming, update model size based on screen, and auto rotate

--- src/components/ThreeD.jsx
+++ src/components/ThreeD.jsx
@@ -1,35 +1,54 @@
 import "./ThreeD.css";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import { useLoader} from "@react-three/fiber"; //, useThree } from "@react-three/fiber";
-import {OrbitControls } from "@react-three/drei";
+import { OrbitControls, OrthographicCamera, PresentationControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { Suspense } from "react";
 
 
 const Model = () => {
-  const gltf = useLoader(GLTFLoader, "./models/rmpg_centered.gltf");
+  const gltf = useLoader(GLTFLoader, "./models/rmpg_upright.gltf");
   return (
     <>
-      <primitive object={gltf.scene} scale={30} />
+      <primitive object={gltf.scene} scale={300} />
     </>
   );
 };
-/*
-const UpdateState = () => {
-  const state = useThree()
-  state.camera.maxPolarAngle:3;
-}*/
+
+function UpdateModelSize() {
+  let width = window.innerWidth;
+  const cam = useThree().camera;
+  cam.zoom = 10 * width/1980;
+}
+
 
 export default function ThreeD() {
+  let width = window.innerWidth;
+  window.addEventListener('resize', () => {
+    UpdateModelSize()
+  });
   return(
     <div className="canvas-container">
       <Canvas className='canvas'>
         <Suspense fallback={null}>
-          {/*<UpdateState />*/}
+          <OrthographicCamera makeDefault zoom={10 * width/1980} near={-200}/>
           <ambientLight intensity={0.2} />
-          <directionalLight color="white" position={[0, 0, 5]} />
-          <Model />
-          <OrbitControls autoRotate autoRotateSpeed={0} enableZoom={true} enablePan={false} minPolarAngle={1} maxPolarAngle={2} minDistance={5} maxDistance={15}/>
+          <directionalLight color="white" position={[0, 100, 20]} />
+          <PresentationControls 
+            enabled={true} // the controls can be disabled by setting this to false
+            global={true} // Spin globally or by dragging the model
+            cursor={false} // Whether to toggle cursor style on drag
+            snap={true} // Snap-back to center (can also be a spring config)
+            speed={2} // Speed factor
+            zoom={0.8} // Zoom factor when half the polar-max is reached
+            rotation={[0, 0, 0]} // Default rotation
+            polar={[-Math.PI / 6, Math.PI / 6]} // Vertical limits
+            azimuth={[-Infinity, Infinity]} // Horizontal limits
+            config={{ mass: 1, tension: 170, friction: 26 }} // Spring config
+          >
+            <Model />
+          </PresentationControls>
+          <OrbitControls autoRotate autoRotateSpeed={1}/>
         </Suspense>
       </Canvas>
     </div>
